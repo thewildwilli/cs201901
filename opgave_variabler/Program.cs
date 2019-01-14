f3249d1e7e66d1c4b0ca7c68de7d516b992003f9
admin <admin@TA-829-08>
2019-01-14 15:52:52 +0100

    oprettet filerne i forbindelse med kursus

--- opgave_variabler/Program.cs
+++ opgave_variabler/Program.cs
@@ -10,6 +10,38 @@ namespace opgave_variabler
     {
         static void Main(string[] args)
         {
+
+            opgave_første();
+
+        }
+
+
+        public void opgave_første()
+        {
+
+            // opgave: https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/Variabler-tal/Opgave/index.md
+
+
+            Int32 heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine(heltal);
+            Console.WriteLine($"Heltal = {heltal}");
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine($"Kommatal = {kommatal}");
+            
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
         }
     }
 }
