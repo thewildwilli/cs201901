a1ca13290b41e6644036e502c5493155e6ede36c
william.larsen <william.larsen@L423>
2019-01-22 09:11:09 +0100

    Added a dice class

--- opgave_klasser_simpel/Terning.cs
+++ opgave_klasser_simpel/Terning.cs
@@ -4,15 +4,48 @@ using System.Text;
 
 namespace opgave_klasser_simpel
 {
-  public class Terning
-  {
-        private int værdi;
-
-    public Terning(int værdi)
+    public class Terning
     {
-            this.Værdi = værdi;
-    }
+        private int værdi;
+        private bool snyd;
+        private static System.Random rnd = new System.Random();
 
-    public int Værdi { get => this.værdi; set => this.værdi = value; }
-  }
+        internal int Skriv()
+        {
+            return this.værdi;
+        }
+
+        internal int Ryst()
+        {
+            switch (this.snyd)
+            {
+                case true:
+                    {
+                        return 6;
+                    }
+                case false:
+                    {
+                        return rnd.Next(1, 7);
+                    }
+                default:
+                    return 1;
+            }
+        }
+
+        public Terning()
+        {
+            this.værdi = 1;
+            this.snyd = false;
+        }
+
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+
+        public int Værdi
+        {
+            get => this.værdi; set => this.værdi = value;
+        }
+    }
 }
